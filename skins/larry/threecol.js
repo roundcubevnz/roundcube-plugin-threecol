8e3aad18d6b038eb4279cd1bf28f471e5c40f991
James Buncle <jbuncle@names.co.uk>
2015-12-21 16:15:55 +0000

    Updated to only apply the threecol layout when the preview pane is visible - to allow other layout plugins  (e.g. mobile layout) to render normally.

--- skins/larry/threecol.js
+++ skins/larry/threecol.js
@@ -1,25 +1,24 @@
 window.rcmail && rcmail.addEventListener('init', function (evt) {
-	$(document.body).addClass('vertical-mail-view');
-	var splitter = rcube_splitter.get_instance('mailviewsplitter');
+	if ($('#mailview-bottom').is(':visible')) {
+		$(document.body).addClass('vertical-mail-view');
+		var splitter = rcube_splitter.get_instance('mailviewsplitter');		//Tweak layouts ready for splitting
+		$('#mailview-bottom').css('top', 0);
+		$('#mailview-top').css('height', 'auto');
 
-	//Tweak layouts ready for splitting
-	$('#mailview-bottom').css('top', 0);
-	$('#mailview-top').css('height', 'auto');
+		//Remove the horizontal splitter handle
+		splitter.handle.remove();
+		//Set options
+		splitter.horizontal = false;
+		splitter.offset = 0;
+		splitter.min = 0;
+		splitter.relative = 1;
+		//Reinitialise
+		splitter.init();
+		splitter.resize();
 
-	//Remove the horizontal splitter handle
-	splitter.handle.remove();
-	//Set options
-	splitter.horizontal = false;
-	splitter.offset = 0;
-	splitter.min = 0;
-	splitter.relative = 1;
-	//Reinitialise
-	splitter.init();
-	splitter.resize();
-
-	
-	//Removed fixed header as it overflows unpleasantly
-	$('#mailview-top .messagelist.fixedheader').first().hide();
-	// Move the pagination controls to allow use in smaller widths
-	$('#countcontrols').prependTo($('#countcontrols').parent());
+		//Removed fixed header as it overflows unpleasantly
+		$('#mailview-top .messagelist.fixedheader').first().hide();
+		// Move the pagination controls to allow use in smaller widths
+		$('#countcontrols').prependTo($('#countcontrols').parent());
+	}
 });
